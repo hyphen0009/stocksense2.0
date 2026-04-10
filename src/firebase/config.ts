6768746cd6ddd86052a47703a553363e90f9d7bf
Hyphen - <hyphen0009@gmail.com>
2026-04-10 05:46:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1463514637-841bb",
+  "appId": "1:981629296706:web:0d51280fc2b61aca9e8ddb",
+  "apiKey": "AIzaSyCXTTWFTlwicEJMe6qS8LnJ-9G8ePmdyTM",
+  "authDomain": "studio-1463514637-841bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "981629296706"
+};
